acaac981e606c0872c7fea9a45d4542c3e6b0fba
Serdar Sara <serdarsara45@gmail.com>
2025-02-19 15:41:24 +0300

    some of the models created

---
+++ Core/CarBook.Domain/Entities/Car.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CarBook.Domain.Entities
+{
+    public class Car
+    {
+        public int CarID { get; set; }
+        public int BrandID { get; set; }  //BrandID is a foreign key that links each car to a specific Brand.
+
+        public Brand Brand { get; set; } //Brand navigation property allows access to the related Brand object.
+
+        public string Model { get; set; }
+
+        public string CoverImageUrl { get; set; }
+
+        public int Km { get; set; }
+
+        public string Transmission { get; set; }
+
+        public byte Seat { get; set; }
+
+        public byte Luggage { get; set; }
+
+        public string Fuel { get; set; }
+
+        public string BigImageUrl { get; set; }
+
+        public List<CarFeature> CarFeatures { get; set; }  // write it into both of car and feature classes
+        public List<CarDescription> CarDesccriptions { get; set; }
+
+        public List<CarPricing> CarPricings { get; set; }
+
+
+
+    }
+}
---
+++ Core/CarBook.Domain/Entities/CarDescription.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CarBook.Domain.Entities
+{
+    public class CarDescription  // descriptionlar uzun olacağımiçin ayri bir tabloda tututldu
+    {
+        public int CarDescriptionID { get; set; }
+        public string Details { get; set; }
+
+        // which car  has this description 
+        public int CarID { get; set; }
+        public Car Car { get; set; }
+
+
+    }
+}
